59d8399680418cf1b6ddc7b977b96db2769f176d
Megamind04 <v.karceski@gmail.com>
2018-01-02 12:06:28 +0100

    Initial commit

---
+++ LendADogDemo.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LendADogDemo.MVC.Startup))]
+namespace LendADogDemo.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
